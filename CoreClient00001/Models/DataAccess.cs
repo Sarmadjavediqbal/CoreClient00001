431b5992d7422ce689dbc2f2a273365fd8d72509
Sarmad Javed <46562878+Sarmadjavediqbal@users.noreply.github.com>
2023-07-15 19:23:12 +0500

    Update 0.0.1-2

--- CoreClient00001/Models/DataAccess.cs
+++ CoreClient00001/Models/DataAccess.cs
@@ -5,12 +5,11 @@ namespace CoreClient00001.Models
 {
     public class DataAccess
     {
-        [Key]
-        [Column("uniqueId")]
-        public int UniqueId { get; set; }
-
         [Column("id")]
         public int Id { get; set; }
+
+        [Column("sensor_id")]
+        public int SensorId { get; set; }
 
         [Column("sensor_data")]
         public double? SensorData { get; set; }
